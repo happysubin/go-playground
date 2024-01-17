56e286c9e31810fd7f6a6481b4f2aa1df7f7f8ba
happysubin <asb0711@gmail.com>
2024-01-17 19:36:10 +0900

    go 모듈 기초 학습 v2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module app
+module example/username/repo //git mod init {여기에 넣는 내용}
 
 go 1.21.5
---
+++ hello_test.go
@@ -0,0 +1,12 @@
+package hello
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	want := "Hello, world."
+	if got := Hello(); got != want {
+		t.Errorf("Hello() = %q, want %q", got, want)
+	}
+}
+
+// go test는 _test로 끝나는 .go 파일을 실행시킨다.
